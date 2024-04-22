0a8c45e0d22f266e34c6db05d6cbdeceeee69e42
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-04-22 07:45:23 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,208 +1,87 @@
-//Buttons(Elevated/Iconbutton/TextButton,Gesturedetection,Inkwell)//Text Field,richText
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: ('Hello World'),
-      home: Home(),
+      home: const Home(),
+      theme: ThemeData(),
     );
   }
 }
 
-//colum=Vertically
-//Row=Horizontally
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
+    //MediaQuery
+    print(MediaQuery.of(context).size);
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).orientation);
+    print(MediaQuery.of(context).displayFeatures);
+    print(MediaQuery.of(context).devicePixelRatio);
+    print(MediaQuery.displayFeaturesOf(context));
+    print(MediaQuery.sizeOf(context));
+
     return Scaffold(
-        backgroundColor: Colors.grey.shade100,
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text(
-            'Home Screen',
-            style: TextStyle(color: Colors.white),
-          ),
-          leading: Icon(Icons.home_filled),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  //ScaffoldMessenger.of(context).showSnackBar(
-                  //SnackBar(content: Text('You Are Gone'),
-                  //backgroundColor: Colors.lightBlue,
-                  //duration: Duration(seconds: 1),
-                  //)
-                  //);
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title:
-                              Text('Still Hope Something is BEtter Coocking'),
-                        );
-                      });
-                },
-                icon: Icon(Icons.add)),
-          ],
-        ),
-        //backgroundColor: Colors.black54,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height:100,
-                width:100,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(22),
-                  border: Border.all(color: Colors.black,width: 3),
-                  image: DecorationImage(
-                    image: AssetImage("images/download.jpeg"),
-                    fit: BoxFit.cover,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color:Colors.black.withOpacity(0.8),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0,3)
-
-
-
-                    ),
-                  ]
-
-                ),
-                alignment: Alignment.center,
-                child: Text('Cat',style: TextStyle(
-                  color: Colors.black,fontWeight: FontWeight.bold
-                ),),
-              )
-              /*ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(
-                          width: 2,
-                          color: Colors.purple,
-                        )),
-                    textStyle: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    padding: EdgeInsets.all(15),
-                  ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
-                  child: Text('Pressed Here')),
-              TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.purple,
-                    textStyle: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Text('TextButton')),
-              IconButton(
-                onPressed: () {
-                  print('Alarm');
-                },
-                icon: Icon(Icons.access_time_filled_outlined),
-                color: Colors.deepOrange,
-              ),
-              GestureDetector(
-                onTap: () {
-                  print('on Tap detected');
-                },
-                onDoubleTap: () {
-                  print('Double Tap');
-                },
-                child: Column(
-                  children: [
-                    Text(" Button"),
-                    Icon(Icons.add_a_photo),
-                    Row(
-                      children: [
-                        Text('World In the Air'),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              InkWell(
-                splashColor: Colors.deepOrange,
-                radius: 15,
-                onTap: () {
-                  print('on Tap detected');
-                },
-                onDoubleTap: () {
-                  print('Double Tap');
-                },
-                child: Column(
-                  children: [
-
-                    Text(" Button"),
-                    Icon(Icons.add_a_photo),
-                    Row(
-                      children: [
-                        Text('World In the Air'),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(16),
-                child: TextField(
-                  //controller: TextEditingController(),
-                  //enabled: false,
-                  maxLength: 15,
-                  style: TextStyle(
-
-                    fontSize: 23,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.orange,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Email",
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(width: 8,color: Colors.orange),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 4,color: Colors.deepOrange),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    enabledBorder:OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(width: 8,color: Colors.red),
-                  ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(width: 8,color: Colors.green),
-
-                ),
-                    fillColor: Colors.purple,
-                    prefixIcon: Icon(Icons.add_a_photo),
-              )))
-
-                    */
-            ]
-          ),
-        ));
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Home'),
+      ),
+      // body: Center(
+      //   child: Wrap(
+      //     alignment: WrapAlignment.center,
+      //     crossAxisAlignment: WrapCrossAlignment.start,
+      //     spacing: 4,
+      //     children: [
+      //       Text(MediaQuery.orientationOf(context).toString()),
+      //       Text(MediaQuery.orientationOf(context).toString()),
+      //       Text(MediaQuery.orientationOf(context).toString()),
+      //       Text(MediaQuery.orientationOf(context).toString()),
+      //     ],
+      //   ),
+      // body: LayoutBuilder(
+      //   builder: (BuildContext context, BoxConstraints constraints) {
+      //     // return Center(
+      //     //   child: Text(constraints.maxHeight.toString()),
+      //     // );
+      //     if(constraints.maxWidth <400){
+      //       return const Center(child: Text('Mobile'),);
+      //
+      //     }else if(constraints.maxWidth <600){
+      //       return const Center(child: Text('Tab'),);
+      //     }else if(constraints.maxWidth <800){
+      //       return const Center(child: Text('Laptop'),);
+      //     }
+      //     return const Center(child: Text('Desktop'),);
+      //   },
+      // ),
+      body: OrientationBuilder(
+        builder: (context,orientation){
+          if(orientation==Orientation.landscape){
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.green,
+            );
+          }else{
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.blueGrey,
+            );
+          }
+        },
+      ),
+    );
   }
 }
