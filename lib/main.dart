065496e4b8ef055fe18849c17ef2f522b3ab2fab
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-04-25 08:04:31 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,72 @@
+// Responsive Builder
+
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+final lastpic= List.generate(24, (index) => 'https://picsum.photos/300/300?random=$index');
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const Home(),
-      theme: ThemeData(),
+      title: 'Responsive Builder',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: MyHome(),
+
     );
   }
 }
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
+class MyHome extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    //MediaQuery
-    print(MediaQuery.of(context).size);
-    print(MediaQuery.of(context).size.height);
-    print(MediaQuery.of(context).orientation);
-    print(MediaQuery.of(context).displayFeatures);
-    print(MediaQuery.of(context).devicePixelRatio);
-    print(MediaQuery.displayFeaturesOf(context));
-    print(MediaQuery.sizeOf(context));
+    return SafeArea(child: Scaffold(
+      body: ResponsiveBuilder(
+        builder:(context,sizeInfo){
+          return GridView.builder(
+            itemCount: lastpic.length,
+            itemBuilder: (context,index)=>
+            CachedNetworkImage(imageUrl: lastpic[index],imageBuilder: (context,provider)=>Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: provider,
+                  fit: BoxFit.cover
+                )
+              ),
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('Home'),
-      ),
-      // body: Center(
-      //   child: Wrap(
-      //     alignment: WrapAlignment.center,
-      //     crossAxisAlignment: WrapCrossAlignment.start,
-      //     spacing: 4,
-      //     children: [
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //     ],
-      //   ),
-      // body: LayoutBuilder(
-      //   builder: (BuildContext context, BoxConstraints constraints) {
-      //     // return Center(
-      //     //   child: Text(constraints.maxHeight.toString()),
-      //     // );
-      //     if(constraints.maxWidth <400){
-      //       return const Center(child: Text('Mobile'),);
-      //
-      //     }else if(constraints.maxWidth <600){
-      //       return const Center(child: Text('Tab'),);
-      //     }else if(constraints.maxWidth <800){
-      //       return const Center(child: Text('Laptop'),);
-      //     }
-      //     return const Center(child: Text('Desktop'),);
-      //   },
-      // ),
-      body: OrientationBuilder(
-        builder: (context,orientation){
-          if(orientation==Orientation.landscape){
-            return Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.green,
-            );
-          }else{
-            return Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.blueGrey,
-            );
-          }
+            ),
+              placeholder: (context,url)=>Center(
+                child: CircularProgressIndicator(),
+              ),
+              errorWidget:(context,url,err)=>Icon(Icons.error) ,
+              ),
+
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: sizeInfo.deviceScreenType == DeviceScreenType.desktop ? 6
+                :sizeInfo.deviceScreenType == DeviceScreenType.tablet ? 3
+                :sizeInfo.deviceScreenType == DeviceScreenType.watch? 1:2,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 4
+            ),
+          );
         },
       ),
-    );
+    ));
   }
 }
+
