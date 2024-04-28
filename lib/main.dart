9b9fa0cd893a6e94a12b2bc73f164cf4624cd660
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-04-28 07:07:18 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,22 @@
-//TExt/Materiall App/Scaffold/Center/Image/Icon
+//Navigation-ush,pop,replace,removeUntill(without named Navigation)
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/home.dart';
 
-void main() {
+
+
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: ('Hello World'),
       home: Home(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        //backgroundColor: Colors.black54,
-        body: Center(
-            /*  child: Text(
-            'Hello World. We are starting our new Flutter Applicationn Today.Everyone Welcome oue new Application',
-            textAlign: TextAlign.center,
-            maxLines: 2,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
-              backgroundColor: Colors.purple,
-              color: Colors.blue,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-
-         */
-            //child: Icon(Icons.android,size: 84,color: Colors.blue,),
-          //child: Image.network('https://images.app.goo.gl/NPMLaUwzh6JeKXSt7'),
-          child: Image.asset('images/download.jpeg',width: 300,height: 300,fit: BoxFit.cover,),
-            ));
-  }
-}
